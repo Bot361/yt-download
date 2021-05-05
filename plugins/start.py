0c1149018aaedebd54d30d49abef7abb4a9d0343
Godzilla BOTs <82944264+Bot361@users.noreply.github.com>
2021-05-05 12:34:33 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton =                 [[
+                                        InlineKeyboardButton(
+                                            "Help", callback_data="help"),
+                                        InlineKeyboardButton(
+                                            "Channel", url="https://t.me/Godzilla_bots")
+                                    ],[
+                                      InlineKeyboardButton(
+                                            "Admin S E N U L A™ ✔️", url="https://t.me/Senulatr")
+                                    ]]
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
