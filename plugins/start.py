a7c17dc8c27e25a0ecfe5cda31cbca8984667bd2
Godzilla BOTs <82944264+Bot361@users.noreply.github.com>
2021-05-05 13:10:49 +0530

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -6,13 +6,13 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton(
-                                            "Help", callback_data="help"),
+                                            "Support Group", url="https://t.me/Musicwold20210"),
                                         InlineKeyboardButton(
                                             "Channel", url="https://t.me/Godzilla_bots")
                                     ],[
                                       InlineKeyboardButton(
                                             "Admin S E N U L A™ ✔️", url="https://t.me/Senulatr")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"හායි <b>{message.from_user.first_name}</b>\n/help වැඩි විස්තර සඳහා"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
