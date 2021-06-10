60be4d94ce8bf44727733006994eb669c58c1c07
Jaindu Charindith <75799963+charindithjaindu@users.noreply.github.com>
2021-06-10 07:23:21 +0530

    Update ytdlfunc.py

--- helper/ytdlfunc.py
+++ helper/ytdlfunc.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 import youtube_dl
 from utils.util import humanbytes
 import asyncio
