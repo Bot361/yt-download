366f410ad4f35521267cf405ad7cb636d35f6788
Jaindu Charindith <75799963+charindithjaindu@users.noreply.github.com>
2021-06-10 07:34:17 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-from pyrogram import Client, filters, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot import user_time
 from config import youtube_next_fetch
 from helper.ytdlfunc import extractYt, create_buttons
