7779847e0ad76274dc171ac56cd39d34a744b5a0
Jaindu Charindith <75799963+charindithjaindu@users.noreply.github.com>
2021-06-10 07:50:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,13 +8,11 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton(
-                                            "Support Group", url="https://t.me/Musicwold20210"),
-                                        InlineKeyboardButton(
-                                            "Channel", url="https://t.me/Godzilla_bots")
+                                            "Repo", url="https://github.com/charindithjaindu/yt-download/")
                                     ],[
                                       InlineKeyboardButton(
-                                            "Admin S E N U L A™ ✔️", url="https://t.me/Senulatr")]
+                                            "Charindith", url="https://t.me/charindith")]
     ])
-    welcomed = f"හායි <b>{message.from_user.first_name}</b>\n/help වැඩි විස්තර සඳහා"
+    welcomed = f"Hello <b>{message.from_user.first_name}</b>\nFor more information use /help"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
